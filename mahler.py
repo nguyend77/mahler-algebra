25b6c99e4d09154770e37a57d3155ed299b71e32
nguyed77 <dung.nguyen@365.elmhurst.edu>
2025-11-16 21:49:31 -0600

    first commit

---
+++ mahler.py
@@ -0,0 +1,108 @@
+class Mahler: # {m: coeff}
+    def __init__(self, terms):
+        self.terms = {m: coeff for m, coeff in terms.items() if coeff != 0}
+    def __repr__(self): # consistent print() representation
+        if not self.terms:
+            return '0'
+        parts = []
+        for m in sorted(self.terms.keys()):
+            coeff = self.terms[m]
+            if not parts: # first term
+                if coeff == 1:
+                    parts.append(f'[{m}]')
+                elif coeff == -1:
+                    parts.append(f'-[{m}]')
+                else:
+                    parts.append(f'{coeff}[{m}]')
+            else: # subsequent terms
+                if coeff == 1:
+                    parts.append(f'+ [{m}]')
+                elif coeff == -1:
+                    parts.append(f'- [{m}]')
+                else:
+                    parts.append(f'{coeff:+} [{m}]')
+        return ' '.join(parts)
+    def __eq__(self, other): # equality check with ==
+        if isinstance(other, int) and other == 0:
+            return not self.terms
+        return self.terms == other.terms
+    def __add__(self, other): # addition with +
+        new_terms = self.terms.copy()
+        for m, coeff in other.terms.items():
+            new_terms[m] = new_terms.get(m, 0) + coeff
+        return Mahler(new_terms)
+    def __sub__(self, other): # subtraction with -
+        new_terms = self.terms.copy()
+        for m, coeff in other.terms.items():
+            new_terms[m] = new_terms.get(m, 0) - coeff
+        return Mahler(new_terms)
+    def __mul__(self, other): # multiplication with *
+        if isinstance(other, int): # scalar multiplication
+            return Mahler({m: coeff * other for m, coeff in self.terms.items()})
+        new_terms = {}
+        for m_self, coeff_self in self.terms.items():
+            for m_other, coeff_other in other.terms.items():
+                m = m_self * m_other
+                coeff = coeff_self * coeff_other
+                new_terms[m] = new_terms.get(m, 0) + coeff
+        return Mahler(new_terms)
+    def __rmul__(self, other): # hanlde scalar multiplication from the left
+        return self.__mul__(other)
+    def __mod__(self, other): # reduce modulo d with %
+        d = other
+        new_terms = {}
+        for m, coeff in self.terms.items():
+            new_terms[m%d] = new_terms.get(m%d, 0) + coeff
+        return Mahler(new_terms)
+    def apply(self, f): # [m](f(x)) = f(x^m)
+        variables = f.variables()
+        if not variables:
+            return sum(self.terms.values()) * f
+        x = variables[0]
+        result = 0
+        for m, coeff in self.terms.items():
+            result += coeff * f.subs({x: x**m})
+        return result
+        
+def moebius_py(n):
+    if n == 1:
+        return 1 # mu(n) = 1  if n = 1
+    prime_factor_count = 0
+    temp_n = n
+    if temp_n % 2 == 0:
+        prime_factor_count += 1
+        temp_n //= 2
+        if temp_n % 2 == 0:
+            return 0 # mu(n) = 0  if n has a squared prime factor
+    i = 3
+    while i * i <= temp_n:
+        if temp_n % i == 0:
+            prime_factor_count += 1
+            temp_n //= i
+            if temp_n % i == 0:
+                return 0 # mu(n) = 0  if n has a squared prime factor
+        i += 2
+    if temp_n > 1: # last prime factor
+        prime_factor_count += 1
+    if prime_factor_count % 2 == 0:
+        return 1
+    else:
+        return -1
+
+def psi(n): # \psi_n = \sum_{d|n} \mu(d)[d]
+    terms = {}
+    divisors = [d for d in range(1,n+1) if n%d == 0]
+    for d in divisors:
+        mu = moebius_py(d)
+        if mu != 0:
+            terms[d] = mu
+    return Mahler(terms)
+
+def phi(n): # \phi_n = sum_{d|n} mu(d)[n/d]
+    terms = {}
+    divisors = [d for d in range(1,n+1) if n%d == 0]
+    for d in divisors:
+        mu = moebius_py(d)
+        if mu != 0:
+            terms[n//d] = mu
+    return Mahler(terms)
